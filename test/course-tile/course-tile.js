652b2b584c55b10ae6f672ea27990f1f046adf55
Ryan Mahler <ryan.mahler@d2l.com>
2016-04-04 16:33:41 -0400

    Add some basic (very basic) tests for the course tile
    Not much behaviour here, so not really much to test...

--- test/course-tile/course-tile.js
+++ test/course-tile/course-tile.js
@@ -1,11 +1,41 @@
-suite('smoke test', function() {
-	var component;
+describe('<course-tile>', function () {
+	var courseTile;
 
-	setup(function() {
-		component = fixture('course-tile-fixture');
+	beforeEach(function () {
+		courseTile = fixture('course-tile-fixture');
 	});
 
-	test('should load', function() {
-		expect(component).to.exist;
+	it('loading element', function () {
+		expect(courseTile).to.exist;
+	});
+
+	describe('defaults', function () {
+		it('has an undefined name', function () {
+			expect(courseTile.name).to.be.undefined;
+		});
+
+		it('has an undefined target', function () {
+			expect(courseTile.target).to.be.undefined;
+		});
+	});
+
+	describe('setting the name attribute', function () {
+		beforeEach(function () {
+			courseTile.name = 'foo';
+		});
+
+		it('should have the name attribute value that was set', function () {
+			expect(courseTile.name).to.equal('foo');
+		});
+	});
+
+	describe('setting the target attribute', function() {
+		beforeEach(function () {
+			courseTile.target = 'http://foo.bar';
+		});
+
+		it('should have the target attribute value that was set', function() {
+			expect(courseTile.target).to.equal('http://foo.bar');
+		});
 	});
 });
