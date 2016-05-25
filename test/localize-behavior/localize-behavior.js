67310271bcdd3d36f4c42d8c7b1335ca1721e68d
d2l-joelp <joel.poiron@d2l.com>
2016-05-25 15:57:55 -0500

    Get locale from `lang` attributes on document

--- test/localize-behavior/localize-behavior.js
+++ test/localize-behavior/localize-behavior.js
@@ -1,15 +1,25 @@
-/* global describe, it, expect, fixture */
+/* global describe, it, expect, fixture, beforeEach */
 
 describe('localize behavior', function() {
 	var component;
 
+	beforeEach(function () {
+		document.documentElement.removeAttribute('lang');
+	});
+
 	it('should have default locale', function () {
-		component = fixture('no-locale-fixture');
+		component = fixture('default-fixture');
+
+		expect(component.locale).to.equal('en-us');
 		expect(component.localize('allCourses')).to.equal('All Courses');
 	});
 
 	it('should use default locale if provided locale does not exist', function () {
-		component = fixture('unsupported-locale-fixture');
+		document.documentElement.setAttribute('lang', 'zz-ZZ');
+
+		component = fixture('default-fixture');
+
+		expect(component.locale).to.equal('zz-ZZ');
 		expect(component.localize('allCourses')).to.equal('All Courses');
 	});
 
