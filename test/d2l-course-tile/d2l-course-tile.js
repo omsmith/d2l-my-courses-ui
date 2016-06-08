97b9aedcb3dc40727e327ff439632ba14f184ea9
Ryan Mahler <ryan.mahler@d2l.com>
2016-06-08 15:07:20 -0400

    Update icons to use new icon name

--- test/d2l-course-tile/d2l-course-tile.js
+++ test/d2l-course-tile/d2l-course-tile.js
@@ -110,9 +110,9 @@ describe('<d2l-course-tile>', function () {
 			var pinIcon = courseTile.$$('.menu-item iron-icon.menu-icon');
 
 			courseTile._pinned = false;
-			expect(pinIcon.icon).to.equal('vui-tier1:pin-filled');
+			expect(pinIcon.icon).to.equal('d2l-tier1:pin-filled');
 			courseTile._pinned = true;
-			expect(pinIcon.icon).to.equal('vui-tier1:pin-hollow');
+			expect(pinIcon.icon).to.equal('d2l-tier1:pin-hollow');
 		});
 
 		it('should call the pinning API', function (done) {
