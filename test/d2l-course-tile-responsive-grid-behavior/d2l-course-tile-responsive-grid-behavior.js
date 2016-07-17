09c854f05df77c2c1754685b2c8d233cf5e53fb0
Ryan Mahler <ryan.mahler@d2l.com>
2016-07-16 22:01:38 -0400

    Simplify how column recalculation is done
    Rather than calling into and out of various elements to manually trigger their `_rescaleCourseTileRegions` method, instead just fire the the `recalculate-columns` event whenever something happens that warrants recalculating how many columns should be displayed. Currently this includes:
    
    - window resize
    - receiving enrollments
    - opening all courses overlay
    - closing all courses overlay
    
    It explicitly does *not* include when a tile is actually moved from one state to the other - the reason being, I'm not sure if this would be a jarring experience. This does mean that, if you're unpinning/unpinning courses, the number of columns won't change (until a refresh, resize, or if you're doing it in the all courses overlay and you close it).

---
+++ test/d2l-course-tile-responsive-grid-behavior/d2l-course-tile-responsive-grid-behavior.js
@@ -0,0 +1,120 @@
+/* global describe, it, expect, fixture, beforeEach, sinon */
+
+'use strict';
+
+describe('d2l-course-tile-responsive-grid-behavior', function() {
+	var component;
+
+	beforeEach(function() {
+		component = fixture('default-fixture');
+	});
+
+	describe('column calculations', function() {
+		it('should properly respond to a window resize event', function(done) {
+			sinon
+				.stub(component, 'getCourseTileItemCount')
+				.returns(1);
+			sinon
+				.stub(component, '_getAvailableWidth')
+				.returns(1024);
+
+			window.dispatchEvent(new Event('resize'));
+
+			// Because of the throttled event listener, need to allow some time
+			setTimeout(function() {
+				expect(component._numCols).to.equal(2);
+				done();
+			}, 100);
+		});
+
+		it('should properly respond to a "recalculate-columns" event', function() {
+			sinon
+				.stub(component, 'getCourseTileItemCount')
+				.returns(1);
+			sinon
+				.stub(component, '_getAvailableWidth')
+				.returns(1024);
+
+			window.dispatchEvent(new Event('recalculate-columns'));
+
+			expect(component._numCols).to.equal(2);
+		});
+
+		it('should have the correct number of columns based on the number of course tiles', function() {
+			[{
+				width: 767,
+				itemCount: 0,
+				expectedColumns: 1
+			}, {
+				width: 767,
+				itemCount: 3,
+				expectedColumns: 1
+			}, {
+				width: 767,
+				itemCount: 4,
+				expectedColumns: 2
+			}, {
+				width: 991,
+				itemCount: 0,
+				expectedColumns: 3
+			}, {
+				width: 991,
+				itemCount: 1,
+				expectedColumns: 2
+			}, {
+				width: 991,
+				itemCount: 2,
+				expectedColumns: 2
+			}, {
+				width: 991,
+				itemCount: 3,
+				expectedColumns: 3
+			}, {
+				width: 991,
+				itemCount: 4,
+				expectedColumns: 2
+			}, {
+				width: 991,
+				itemCount: 5,
+				expectedColumns: 3
+			}, {
+				width: 992,
+				itemCount: 0,
+				expectedColumns: 4
+			}, {
+				width: 992,
+				itemCount: 1,
+				expectedColumns: 2
+			}, {
+				width: 992,
+				itemCount: 2,
+				expectedColumns: 2
+			}, {
+				width: 992,
+				itemCount: 3,
+				expectedColumns: 3
+			}, {
+				width: 992,
+				itemCount: 4,
+				expectedColumns: 4
+			}, {
+				width: 992,
+				itemCount: 5,
+				expectedColumns: 3
+			}, {
+				width: 992,
+				itemCount: 6,
+				expectedColumns: 3
+			}, {
+				width: 992,
+				itemCount: 7,
+				expectedColumns: 4
+			}]
+			.forEach(function(scenario) {
+				var description = 'width: ' + scenario.width + '; itemCount: ' + scenario.itemCount;
+				var numberOfColumns = component._calcNumColumns(scenario.width, scenario.itemCount);
+				expect(numberOfColumns, description).to.equal(scenario.expectedColumns);
+			});
+		});
+	});
+});
