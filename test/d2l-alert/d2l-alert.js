83cece6f0e1b961a3843052d19ed569e3a053fc8
Dave Harris <david.harris@d2l.com>
2016-11-23 15:31:23 -0600

    rename alert-type to type

--- test/d2l-alert/d2l-alert.js
+++ test/d2l-alert/d2l-alert.js
@@ -13,12 +13,12 @@ describe('d2l-alert', function() {
 		expect(component.innerHTML).to.contain('Alert text');
 	});
 
-	it('should apply the call-to-action alert-type by default', function() {
-		expect(component.attributes.getNamedItem('alert-type').value).to.equal('call-to-action');
+	it('should apply the call-to-action alert type by default', function() {
+		expect(component.attributes.getNamedItem('type').value).to.equal('call-to-action');
 	});
 
-	it('should apply the alert-type property value as an attribute', function() {
-		component['alert-type'] = 'reinforcement';
-		expect(component.attributes.getNamedItem('alert-type').value).to.equal('reinforcement');
+	it('should apply the alert type property value as an attribute', function() {
+		component['type'] = 'reinforcement';
+		expect(component.attributes.getNamedItem('type').value).to.equal('reinforcement');
 	});
 });
