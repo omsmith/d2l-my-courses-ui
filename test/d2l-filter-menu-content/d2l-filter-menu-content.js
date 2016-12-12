4fcd05190f16272bd3cff17e4f6c66e304a69ed0
Ryan Mahler <ryan.mahler@d2l.com>
2016-12-12 16:33:25 -0500

    Break out d2l-list-item-filter
    This is going to be used in multiple locations in a future change, so good to break it out now. Jeff called it.

--- test/d2l-filter-menu-content/d2l-filter-menu-content.js
+++ test/d2l-filter-menu-content/d2l-filter-menu-content.js
@@ -46,57 +46,6 @@ describe('d2l-filter-menu-content', function() {
 		expect(widget._searchSemestersAction.href).to.equal('/enrollments');
 	});
 
-	describe('d2l-list-item-filter', function() {
-		var listItem;
-
-		beforeEach(function() {
-			listItem = fixture('d2l-list-item-filter-fixture');
-		});
-
-		it('should change icon when selected state changes', function() {
-			expect(listItem.$$('d2l-icon').icon).to.equal('d2l-tier2:check-box-unchecked');
-
-			listItem.set('selected', true);
-
-			expect(listItem.$$('d2l-icon').icon).to.equal('d2l-tier2:check-box');
-		});
-
-		it('should fetch the organization when the enrollment changes', function() {
-			var stub = sinon.stub(listItem.$$('d2l-ajax'), 'generateRequest');
-
-			listItem.set('enrollmentEntity', parser.parse(enrollment));
-
-			expect(stub.called).to.be.true;
-			expect(listItem._organizationUrl).to.equal('/organizations/1');
-		});
-
-		it('should update text and value based off of organizations response', function(done) {
-			var server = sinon.fakeServer.create();
-			server.respondImmediately = true;
-			server.respondWith(
-				'GET',
-				'/organizations/1',
-				[200, '{}', JSON.stringify({
-					properties: {
-						name: 'foo'
-					},
-					links: [{
-						rel: ['self'],
-						href: 'bar'
-					}]
-				})]);
-
-			listItem.set('enrollmentEntity', parser.parse(enrollment));
-
-			setTimeout(function() {
-				expect(listItem.text).to.equal('foo');
-				expect(listItem.value).to.equal('bar');
-				server.restore();
-				done();
-			});
-		});
-	});
-
 	describe('Visibility', function() {
 		it('should signal that it should be hidden if user does not have enough departments/semesters', function(done) {
 			var handler = function(e) {
